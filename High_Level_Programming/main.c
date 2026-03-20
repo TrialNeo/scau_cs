e9f6aab3ff07aa73b113883817f2019ec2989939
Trial <shenpanpro@gmail.com>
2026-03-20 17:46:51 +0800

    PBM created

---
+++ High_Level_Programming/main.c
@@ -0,0 +1,22 @@
+// Created by Github@TrialNeo(shenpanpro@gmail.com) 
+// Created Time 2026/3/20.
+
+#include <stdio.h>
+
+#include "menu/menu.h"
+#include <stdbool.h>
+int main() {
+    char op = 0;
+    while (true) {
+        menu_welcome();
+        printf("请输入您的功能编号:");
+        op  = getchar();
+
+        switch (op) {
+            case 0:
+                return 0;
+            default:
+                break;
+        }
+    }
+}
