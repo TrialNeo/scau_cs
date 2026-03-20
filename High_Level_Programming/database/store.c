9b6e1351f171c08a7e615a52558544aa69f914ab
Trial <shenpanpro@gmail.com>
2026-03-20 22:05:23 +0800

    简单的link模板

--- High_Level_Programming/database/store.c
+++ High_Level_Programming/database/store.c
@@ -1,7 +1,5 @@
 // Created by Github@TrialNeo(shenpanpro@gmail.com) 
 // Created Time 2026/3/20.
 
-#ifndef USER_DATA_H
-#define USER_DATA_H
+#include "store.h"
 
-#endif
---
+++ High_Level_Programming/database/store.h
@@ -0,0 +1,7 @@
+// Created by Github@TrialNeo(shenpanpro@gmail.com) 
+// Created Time 2026/3/20.
+
+#ifndef SCAU_OJ2_STORE_H
+#define SCAU_OJ2_STORE_H
+
+#endif //SCAU_OJ2_STORE_H
